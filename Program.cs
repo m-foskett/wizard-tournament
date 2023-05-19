abf073f981c213bb822e2b4bd35515da91bd1356
Mark Foskett <markryanfoskett@gmail.com>
2023-05-19 23:49:44 +1000

    basic user interaction

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Versioning;
+
+namespace wizards
+{
+    class Program
+    {
+        [SupportedOSPlatform("windows")]
+        static void Main(string[] args)
+        {
+            // Change console settings
+            Console.Title = "Grand Wizard Tournament";
+            Console.WindowHeight = 40;
+            // NPC Dialog
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello there challenger!\nWelcome to the GRAND WIZARD TOURNAMENT!\n");
+            Console.WriteLine("What might your name be?");
+            // Wait for user response
+            Console.ForegroundColor = ConsoleColor.Red;
+            var name = Convert.ToString(Console.ReadLine());
+            // NPC Dialog
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nSo your name is " + name + "? Hmmm... never heard of you.\nI hope you know what you're getting yourself into.\n");
+            // Wait for user input
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press any key to exit the tournament.");
+            Console.ReadKey();
+        }
+    }
+}
+
